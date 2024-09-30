8b1c45bf7f48859fc8fa9d1b2afa6426a6739db2
meric <mericcintosunn@gmail.com>
2024-10-01 01:27:31 +0300

    fixed-strapi-develop

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,6 +1,6 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'v+1+CXZfD/u3spGbDmrD9Q=='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
